271ea82d89fcd804c2337444d150ef50090800d2
jp1017 <jp8917@163.com>
2015-12-23 09:56:51 +0800

    add usage

--- app/src/main/java/com/example/jp/greendao_demo/MainActivity.java
+++ app/src/main/java/com/example/jp/greendao_demo/MainActivity.java
@@ -5,16 +5,30 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.example.jp.DaoMaster;
+import com.example.jp.DaoSession;
+import com.example.jp.User;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private DaoMaster mDaoMaster;
+    private DaoSession mDaoSession;
+    List<User> users = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //建立数据库，这个最好写到Application中，这里方便就写到了这里
+        buildDatabase();
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -26,6 +40,20 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        for (int i = 0; i < 10; i++) {
+            User user = new User();
+            user.setNumber(i + "");
+            user.setPassword("#####" + i + "*****");
+            users.add(user);
+            mDaoSession.insert(user);
+        }
+    }
+
+    private void buildDatabase() {
+        DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, "user-db", null);
+        mDaoMaster = new DaoMaster(helper.getWritableDatabase());
+        mDaoSession = mDaoMaster.newSession();
     }
 
     @Override
