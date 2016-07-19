8ad46096492b836ad247da0b70b3378149372e37
jp1017 <jp8917@163.com>
2016-07-19 12:36:43 +0800

    update libs to 3.0.0

--- daogen/src/main/java/com/example/DaoGen.java
+++ daogen/src/main/java/com/example/DaoGen.java
@@ -1,14 +1,15 @@
 package com.example;
 
-import de.greenrobot.daogenerator.DaoGenerator;
-import de.greenrobot.daogenerator.Entity;
-import de.greenrobot.daogenerator.Schema;
+
+import org.greenrobot.greendao.generator.DaoGenerator;
+import org.greenrobot.greendao.generator.Entity;
+import org.greenrobot.greendao.generator.Schema;
 
 public class DaoGen {
     public static void main(String args[]) throws Exception {
         //创建了一个用于添加实体（Entity）的模式（Schema）对象。
         Schema schema = new Schema(1, "com.example.jp");
-
+        schema.enableKeepSectionsByDefault();//加入自己的一些信息，下次生成的时候不消失
         //一个实体（类）就关联到数据库中的一张表，此处表名为User（类名）
         Entity userBean = schema.addEntity("User");
 
